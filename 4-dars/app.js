ad68d75b47664d6695bb545a804db3f020f5cd4a
BAHODIR[FE-20] <bahodirjalilov7777@gmail.com>
2023-06-03 18:34:49 +0500

    work4

---
+++ 4-dars/app.js
@@ -0,0 +1,79 @@
+// string metodlari
+const card = document.querySelector(".card");
+
+// function sliced(str, from, to) {
+//   let result = "";
+//   for (let i = from; i < to && i < str.length; i++) {
+//     result += str[i];
+//   }
+//   return result;
+// }
+
+// const myString = "Hello, world!";
+// card.innerHTML = sliced(myString, 0, 5);
+
+// function subStrings(str, from, to) {
+//   let result = "";
+//   for (let i = from; i < to && i < str.length; i++) {
+//     result += str[i];
+//   }
+//   return result;
+// }
+// let Strings = "Bahodir";
+// card.innerHTML = subStrings(Strings, 0, 5);
+
+// function myIndexOf(str, search) {
+//   for (let i = 0; i < str.length - search.length +1; i++) {
+//     if (str.slice(i, i + search.length) === search) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+
+// let result = "Uzbekistan";
+
+// card.innerHTML = myIndexOf(result, "n");
+
+// function myIncluds(str, search) {
+//     let result = false;
+//     for (let i = 0; i < str.length - search.length + 1; i++) {
+//       if (str.slice(i, i + search.length) === search) {
+//         result = true;
+
+//       }
+//     }
+//     return result;
+//   }
+
+//   let answer="Jalilov";
+//   card.innerHTML=myIncluds(answer, "ali")
+
+// function startsWidth(str, search) {
+//   let result = false;
+//   if (str.slice(0, search.length) === search) {
+//     result = true;
+//   }
+
+//   return result;
+// }
+
+// let answer = "open";
+
+// card.innerHTML = startsWidth(answer, "o");
+
+// function endsWidth(str, search) {
+//     let result = false;
+//     if (str.slice(-search.length) === search) {
+//       result = true;
+//     }
+
+//     return result;
+//   }
+
+//   let answer = "open";
+
+//   card.innerHTML = endsWidth(answer, "n");
+
+
+  
