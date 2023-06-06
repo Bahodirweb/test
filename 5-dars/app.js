53d0e8443b24cbcf8610e504713dc1fdbc495a09
BAHODIR[FE-20] <bahodirjalilov7777@gmail.com>
2023-06-06 18:18:44 +0500

    work5

--- 5-dars/app.js
+++ 5-dars/app.js
@@ -1,41 +1,41 @@
 const card = document.querySelector(".card");
 
 // function myPop(arr) {
-//     let lastItem = arr[arr.length - 1]; 
-//     arr.length = arr.length - 1; 
-//     return lastItem; 
+//     let lastItem = arr[arr.length - 1];
+//     arr.length = arr.length - 1;
+//     return lastItem;
 //   }
-  
+
 //   let arr=["Oybek","Bahodir","Bexruz"];
 //   card.innerHTML=myPop(arr);
 //   card.innerHTML=arr;
 
 // function myPush(arr, newElement) {
-//     arr[arr.length] = newElement; 
-//     return arr.length; 
+//     arr[arr.length] = newElement;
+//     return arr.length;
 //   }
 //   let arr=["Oybek","Bahodir","Bexruz"];
 //   card.innerHTML=myPush(arr,"Marat");
 //   card.innerHTML=arr;
 
 // function myShift(arr) {
-//     let firstItem = arr[0]; 
+//     let firstItem = arr[0];
 //     for (let i = 0; i < arr.length - 1; i++) {
-//       arr[i] = arr[i + 1]; 
+//       arr[i] = arr[i + 1];
 //     }
 //     arr.length = arr.length - 11;
-//     return firstItem; 
+//     return firstItem;
 //   }
 //   let arr=["Oybek","Bahodir","Bexruz"];
 //   card.innerHTML=myShift(arr,);
 //   card.innerHTML=arr;
-  
+
 // function myUnshift(arr, newElement) {
 //   for (let i = arr.length; i >= 0; i--) {
-//     arr[i] = arr[i - 1]; 
+//     arr[i] = arr[i - 1];
 //   }
-//   arr[0] = newElement; 
-//   return arr.length; 
+//   arr[0] = newElement;
+//   return arr.length;
 // }
 
 // let arr=["Oybek","Bahodir","Bexruz"];
@@ -49,10 +49,49 @@ const card = document.querySelector(".card");
 //     }
 //     return result;
 //   }
-  
+
 //   let numbers = [12, 25, 39, 48, 57];
 //   let square = myMap(numbers);
 //   card.innerHTML=square;
 
+// function mySplit(str, Bahodir) {
+//     let result = [];
+//     let word = "";
+    
+//     for (let i = 0; i < str.length; i++) {
+//       if (str[i] === Bahodir) {
+//         result.push(word);
+//         word = "";
+//       } 
+//       else {
+//         word += str[i];
+//       }
+//     }
+    
+//     result.push(word);
+//     return result;
+//   }
+  
+//   let myString = "Asqar Marat ";
+//   card.innerHTML=mySplit(myString, " ");
+
+
+
+function myJoin(arr, Bahodir) {
+    let result = "";
+    
+    for (let i = 0; i < arr.length; i++) {
+      result += arr[i];
+      
+      if (i !== arr.length - 1) {
+        result += Bahodir;
+      }
+    }
+    
+    return result;
+  }
+  
+  let myArray = ["Asqar", "Marat","oybek","Jamshit"];
+  card.innerHTML= myJoin(myArray, " , ");
   
   
